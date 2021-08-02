9769894c32774f54c6a208caad5dc4138bdeb5e2
Ivan <ivan@plugion.org>
2021-08-02 20:10:43 +0300

    Create example.js

---
+++ example.js
@@ -0,0 +1,11 @@
+jQuery(document).on( 'wbk_on_form_rendered', function(){
+    if( typeof wbk_get_converted !== 'undefined' ){
+		    jQuery.each( wbk_get_converted, function( key, value ) {
+		        if ( key != 'action' && key != 'time' && key != 'service' && key != 'step' ){
+                if( jQuery( 'input[name="' + key + '"]').length == 1 ){
+                    jQuery( 'input[name="' + key + '"]').val(value);
+                }     
+		        }
+		    });
+    }
+});
